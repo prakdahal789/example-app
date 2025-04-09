5d7e6e9494edb353c63cd40cbe247cb70ad56798
Prakash <prakdahal789@gmail.com>
2025-04-09 09:12:32 +0545

    updated values of cards

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -9,23 +9,23 @@
     </div>
     <div class="bg-blue-50 p-4 rounded-lg shadow-md">
         <h2 class="text-xl font-bold"> Pending Orders</h2>
-        <p class="text-3xl font-bold">150</p>
+        <p class="text-3xl font-bold">200</p>
     </div>
     <div class="bg-blue-50 p-4 rounded-lg shadow-md">
         <h2 class="text-xl font-bold"> Processing Products</h2>
-        <p class="text-3xl font-bold">150</p>
+        <p class="text-3xl font-bold">300</p>
     </div>
     <div class="bg-blue-50 p-4 rounded-lg shadow-md">
         <h2 class="text-xl font-bold"> Total Users</h2>
-        <p class="text-3xl font-bold">150</p>
+        <p class="text-3xl font-bold">400</p>
     </div>
     <div class="bg-blue-50 p-4 rounded-lg shadow-md">
         <h2 class="text-xl font-bold"> Total Categories</h2>
-        <p class="text-3xl font-bold">150</p>
+        <p class="text-3xl font-bold">500</p>
     </div>
     <div class="bg-blue-50 p-4 rounded-lg shadow-md">
         <h2 class="text-xl font-bold"> Total Sales</h2>
-        <p class="text-3xl font-bold">150</p>
+        <p class="text-3xl font-bold">8000</p>
     </div>
 
 </div>
